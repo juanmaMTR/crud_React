0b75a07e831c66f3bafc3b796b75d94055fc288f
juanmaMTR <jtoscanoreyes.guadalupe@alumnado.fundacionloyola.net>
2022-02-04 11:42:50 +0100

    Crate Components

--- src/index.js
+++ src/index.js
@@ -2,31 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    constructor(props){
-        super(props)
-        this.state={
-            value:null
-        }
+class Menu extends React.Component {
+    constructor(){
+        super()
     }
     render() {
       return (
-        <button className="square" onClick={this.props.onClick}>
-          {this.props.value}
-        </button>
+        <div id='menu'>
+            <h1>Div del Menu</h1>
+        </div>
       );
     }
 }
   
-class Board extends React.Component {
-    constructor(props){
-        super(props)
-        this.state={
-            squares: Array(9).fill(null),
-            turnoX:true,
-        }
+class Formulario extends React.Component {
+    constructor(){
+        super()
     }
-    handleClick(i){
+    /*handleClick(i){
         const newSquares=this.state.squares.slice()
         newSquares[i]=this.state.turnoX ? 'X':'O'
         if(this.comprobarGanador(newSquares)){
@@ -37,63 +30,63 @@ class Board extends React.Component {
             turnoX: !this.state.turnoX,
         })
         //console.log(i); //Muestra la posición que se pulsa
-    }
-    comprobarGanador(estado){
-        const filas=[
-            [0,1,2],[3,4,5],[6,7,8], //horizontales
-            [0,3,6],[1,4,7],[2,5,8], //verticales
-            [0,4,8],[2,4,6] //diagonales
-        ]
-        for(let i=0;i<filas.length;i++){
-          if(estado[filas[i][0]] && estado[filas[i][0]]==estado[filas[i][1]] && estado[filas[i][1]]==estado[filas[i][2]]){
-            return true
-          }
-        }
-        return false
-    }
-    renderSquare(i) {
+    }*/
+    /*renderSquare(i) {
       return <Square value={this.state.squares[i]} 
                 onClick={()=>this.handleClick(i)}
             />;
-    }
+    }*/
   
     render() {
-      const status = 'Siguiente jugador: '+(this.state.turnoX ? 'X':'O');
-  
       return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
+        <div id='formulario'>
+            <form>
+                <Campos />
+                <input type="submit" value="Enviar" />
+            </form>
         </div>
       );
     }
 }
-  
-class Game extends React.Component {
+
+class Campos extends React.Component {
+    constructor(){
+        super()
+    }
+    render(){
+        return(
+            <div id='campos'>
+                <label>
+                    Nombre:
+                    <input type="text" placeholder="Nombre"/>
+                </label>
+                <br />
+                <label>
+                    Apellidos:
+                    <input type="text" placeholder="Apellidos"/>
+                </label>
+                <br />
+                <label>
+                    Correo:
+                    <input type="mail" placeholder="Correo"/>
+                </label>
+                <br />
+                <label>
+                    DNI:
+                    <input type="text" placeholder="DNI"/>
+                </label>
+            </div>
+            
+        );
+    }
+}
+
+class Pagina extends React.Component {
     render() {
       return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
+        <div className="pagina">
+          <Menu />
+          <Formulario />
         </div>
       );
     }
@@ -102,6 +95,6 @@ class Game extends React.Component {
   // ========================================
   
 ReactDOM.render(
-    <Game />,
+    <Pagina />,
     document.getElementById('root')
 );
